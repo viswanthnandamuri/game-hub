fc8f82e04d36e3890a157e3c1f642c45ecdcef85
Viswanth Nandamuri <viswanthnandamuri382@gmail.com>
2024-04-30 17:00:32 -0400

    Built Color Mode

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,9 @@ function App() {
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside" bg="gold">
-            ASIDE
-          </GridItem>
+          <GridItem area="aside">ASIDE</GridItem>
         </Show>
-        <GridItem area="main" bg="dodgerblue">
-          main
-        </GridItem>
+        <GridItem area="main">main</GridItem>
       </Grid>
     </>
   );
