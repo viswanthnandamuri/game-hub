b99478abdf3cc58d1219bf8ee8469784da96c751
Viswanth Nandamuri <viswanthnandamuri382@gmail.com>
2024-05-14 15:29:02 -0400

    Displaying Platforms

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+  FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    linux: FaLinux,
+    web: BsGlobe,
+  };
+  return (
+    <>
+      <HStack marginY={1}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
