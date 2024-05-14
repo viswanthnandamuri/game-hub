a130799a2cc02b31c92c5c527b24139bdacef584
Viswanth Nandamuri <viswanthnandamuri382@gmail.com>
2024-05-14 17:12:43 -0400

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,27 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
     <>
-      <ul>
+      <List>
         {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id} paddingY={1}>
+            <HStack objectFit="cover">
+              <Image
+                borderRadius={8}
+                width={8}
+                height={8}
+                src={genre.image_background}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+
+              {/* {genre.image_background} */}
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
